bf2246c5d5bf0adac2c318d1fe4ee4fb13b5414f
kestisk <sezerkesti@gmail.com>
2018-08-17 13:16:50 +0300

    sk

---
+++ parentubeApi/Controllers/atController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace parentubeApi.Controllers
+{
+    public class atController : ApiController
+    {
+        public void sezer()
+        {
+            int a = 5;
+        }
+    }
+}
